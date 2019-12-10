b01a651bdd661a78011d86496a763dde899aa550
C-Cairns <58695406+C-Cairns@users.noreply.github.com>
2019-12-10 09:13:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/*eslint-env browser*/
+
+//function myFunction(p1, p2) {
+//    "use strict";
+//    return p1 * p2;
+//}
+//document.getElementById("demo").innerHTML = myFunction(4, 3);
+//
+//let charName;
+//let charClass;
+//let adventure = true;
+//let charAge;
+//
+//function(charName, charClass, charAge)
+//{
+//
+//}
+
+//var message = "\'Hello\', world";
+//alert(message);
+//
+//let message2 = message + 5;
+//alert(message2);
+//
+//alert(message.length);
+
+var message = "Congrats. The new version comitted.";
+alert(message);
+
+let message2 = message + ". Sorry. Just repeating myself.";
+alert(message2);
+
+alert("FYI the last message is " + message2.length + " characters long.");
